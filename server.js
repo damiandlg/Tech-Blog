ad2e93dba401a3e3894f48286110b4350a9f840a
damiandlg <damian.delagarzaf@gmail.com>
2022-11-11 01:14:12 -0600

    getting errors in user, seeds, and nod-modules. Debug tmw

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'SECRET SECRET SECRET',
-    cookie: (),
+    cookie: {},
     resave: false,
     saveUninitalized: true,
     store: new SequelizeStore({
