47a1d8814b1ae34e62f022cd57c828583c526a7e
damiandlg <damian.delagarzaf@gmail.com>
2022-11-10 22:24:39 -0600

    added code to main.handalbars and added the user model

---
+++ models/user.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+  {
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement:true
+    },
+  
+    username: {
+    type: DataTypes.STRING,
+    allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [6]
+        }
+    }
+},
+
+  {
+    hooks: {
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        async beforeCreate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
+
+    },
+
+    // pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
+const routes = require('./controllers/');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
